eaf25ab368c1a50ca0c8f78fd632c74397f1dbfd
Taylor <c3206230@uon.edu.au>
2018-03-13 22:27:00 +1100

    Questions Now work

--- Ass-01-WINNT/src/Ass-01-Q04.c
+++ Ass-01-WINNT/src/Ass-01-Q04.c
@@ -5,19 +5,65 @@
 #include "Ass-01.h"
 
 int string_parser(char *inp, char **array_of_words_p[]){
-	//
-	printf("I got here 8");
-	char prev_space = 0;
-	char word_start = 1;
-	int No_words = 0;
-	char current_position = 0;
-
-	unsigned char Next_char = 0; //TODO
-	printf("%c\n",Next_char);
-
-	Next_char = scanf("%c", inp);
-	//
 
 	printf("\nCODE TO BE WRITTEN...\n");
-	return 0;
+
+	//read each character in string
+	    char prev_space = 0;
+	    char word_start = 1;
+	    int No_words = 0;
+	    char current_position = 0;
+
+	    char Next_char;
+
+        for(int i=0; i<20; ++i){
+            printf("%c\n", inp[i]);
+
+	        Next_char = inp[i];
+
+	        if(Next_char == 0){    //NULL
+	            if(No_words == 0){
+	                printf("No words in string\n");
+	                return No_words; //?
+	            }
+	            else{
+	                //Words were found
+	                return 0;
+	            }
+	        }
+	        else if(isspace(Next_char) != 0){
+	            prev_space = 1;
+	            //Read next character
+	            current_position++;
+	            printf("Found a space\n");
+	            word_start = 1;
+	            if(No_words<0){
+	                printf("Full word found %d\n", No_words);
+	            }
+	        }
+
+	        //If i get to here must be a character
+	        else{
+	            if(word_start == 1){
+	                //Store number of words
+	                No_words++;
+	                current_position++;
+	                printf("Start of word found\n");
+	                word_start = 0;
+
+	                //Store stream position
+	                //array_of_words_p = ;
+
+	                prev_space = 0;
+	            }
+	            else{
+	                //currently in a word already, do nothing
+	                current_position++;
+	                prev_space = 0;
+	                word_start = 0;
+	            }
+	        }
+	    }
+    printf("Success");
+	return No_words;
 }
