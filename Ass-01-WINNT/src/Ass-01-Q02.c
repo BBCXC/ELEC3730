1b9c3a7e4724070b5ccb4cbf28210b73e50a37f8
Taylor <c3206230@uon.edu.au>
2018-03-18 17:33:42 +1100

    Fixed Q2

--- Ass-01-WINNT/src/Ass-01-Q02.c
+++ Ass-01-WINNT/src/Ass-01-Q02.c
@@ -6,90 +6,93 @@ Description: */
 
 
 #include "Ass-01.h"
+#include <string.h>
 
-int read_pcm_wavefile(pcm_wavefile_header_t *header_p, char **data_p, char *filename)
-{
-	FILE * filevar;		/* variable of type file */
-int flag, i;
-char ArrayCheck1[4] = "RIFF";
-char ArrayCheck2[3] = "fmt";
+int read_pcm_wavefile(pcm_wavefile_header_t *header_p, char **data_p, char *filename){
+
+	FILE* filevar;		/* variable of type file */
+	pcm_wavefile_header_t header;
+	const char* RIFF_string = "RIFF";
+	const char* fmt_string = "fmt";
+	int ui8_t_size = sizeof(uint8_t);
+	int ui16_t_size = sizeof(uint16_t);
+	int ui32_t_size = sizeof(uint32_t);
 
 	filevar = fopen(filename, "rb");										/* opening the file and storing it in file variable */
-
-	fread(header_p->ChunkID, sizeof(uint8_t), 4, filevar);					/* Reading the Chunk ID */
-	i =strncmp(header_p->ChunkID, ArrayCheck1, 4);							/* Checking to see if the ID is correct */
-	if (i != 0) {
-		printf("ERROR! incorrect ChunkID \n");
-		flag = 1;
+	if(filevar == 0){  //If file pointer returns NULL
+		printf("%3s ERROR: File unknown \n", " ");  //Log Error
+		return -1;
 	}
-	fread(&header_p->ChunkSize, sizeof(uint32_t), 1, filevar);
 
-	fread(header_p->Format, sizeof(uint8_t), 4, filevar);
+	fread(header.ChunkID, ui8_t_size, 4, filevar);					/* Reading the Chunk ID */
+	if (strncmp(RIFF_string, (char*)header.ChunkID, 4) != 0){	/* Checking to see if the ID is correct */
+		printf("%3s ERROR: Incorrect ChunkID expected: %s got: %c%c%c%c\n", " ", RIFF_string, header.ChunkID[0], header.ChunkID[1], header.ChunkID[2], header.ChunkID[3]);
+		return -1;
+	}
+	fread(&header.ChunkSize, ui32_t_size, 1, filevar);
 
-	fread(header_p->Subchunk1ID, sizeof(uint8_t), 4, filevar);
-	i = strncmp(header_p ->Subchunk1ID, ArrayCheck2, 3);
-			if (i != 0) {
-				printf("Error! Incorrect Subchunk2ID \n");
-				flag = 1;
-			}
+	fread(header.Format, ui8_t_size, 4, filevar);
 
+	fread(header.Subchunk1ID, ui8_t_size, 4, filevar);
+	if (strncmp(fmt_string, (char*)header.Subchunk1ID, 3) != 0) {
+		printf("%3s ERROR: Incorrect Subchunk1ID expected: %s got: %c%c%c%c\n", " ", fmt_string, header.Subchunk1ID[0], header.Subchunk1ID[1], header.Subchunk1ID[2], header.Subchunk1ID[3]);
+		return -1;
+	}
 
-
-
-	fread(&header_p->Subchunk1Size, sizeof(uint32_t), 1, filevar);
-
-		if (header_p-> Subchunk1Size != 16) {
-			fread(&header_p->AudioFormat, sizeof(uint16_t), 1, filevar);
-				if (header_p->AudioFormat != 1)
-					printf("Error! not PCM encoded \n");
-		flag = 1;
+	fread(&header.Subchunk1Size, ui32_t_size, 1, filevar);
+	if (header. Subchunk1Size != 16) {
+		fread(&header.AudioFormat, ui16_t_size, 1, filevar);
+		if (header.AudioFormat != 1){
+			printf("%3s ERROR: Not PCM encoded \n", " ");
 		}
-		else  {
+		return -1;
+	}
+	else{
+		fread(&header.AudioFormat, ui16_t_size, 1, filevar);
+	}
 
-	fread(&header_p->AudioFormat, sizeof(uint16_t), 1, filevar);
-
-		}
-
-	fread(&header_p->NumChannels, sizeof(uint16_t), 1, filevar);
-	fread(&header_p->SampleRate, sizeof(uint32_t), 1, filevar);
-	fread(&header_p ->ByteRate, sizeof(uint32_t), 1, filevar);
-	fread(&header_p->BlockAlign, sizeof(uint16_t), 1, filevar);
-	fread(&header_p->BitsPerSample, sizeof(uint16_t), 1, filevar);
-	fread(header_p->Subchunk2ID, sizeof(uint8_t), 4, filevar);
-	fread(&header_p->Subchunk2Size, sizeof(uint32_t), 1, filevar);
+	fread(&header.NumChannels, ui16_t_size, 1, filevar);
+	fread(&header.SampleRate, ui32_t_size, 1, filevar);
+	fread(&header_p ->ByteRate, ui32_t_size, 1, filevar);
+	fread(&header.BlockAlign, ui16_t_size, 1, filevar);
+	fread(&header.BitsPerSample, ui16_t_size, 1, filevar);
+	fread(header.Subchunk2ID, ui8_t_size, 4, filevar);
+	fread(&header.Subchunk2Size, ui32_t_size, 1, filevar);
 
 
 	/* Print Statements */
-	if (flag != 1) {
-	printf("\n -> ChunkID = %c%c%c%c \n", header_p->ChunkID[0], header_p->ChunkID[1], header_p->ChunkID[2], header_p->ChunkID[3]);
-	printf("-> ChunkSize = %d \n", header_p->ChunkSize);
-	printf("-> Chunk Format = %c%c%c%c \n", header_p->Format[0], header_p->Format[1], header_p->Format[2], header_p->Format[3]);
-	printf(" -> Subchunk1ID = %c%c%c \n", header_p->Subchunk1ID[0], header_p->Subchunk1ID[1], header_p->Subchunk1ID[2]);
-	printf("-> Subchunk1size = %d \n", header_p->Subchunk1Size);
-	printf("-> AudioFormat = %d \n", header_p->AudioFormat);
-	printf(" -> Number of channels = %d \n", header_p ->NumChannels);
-	printf(" -> Sample rate = %d \n", header_p ->SampleRate);
-	printf(" -> ByteRate = %d \n", header_p ->ByteRate);
-	printf(" -> BlockAlign = %d \n", header_p ->BlockAlign);
-	printf(" -> BitsPerSample = %d \n", header_p ->BitsPerSample);
-	printf(" -> SubChuck2ID = %c%c%c%c \n", header_p->Subchunk2ID[0], header_p->Subchunk2ID[1], header_p->Subchunk2ID[2], header_p->Subchunk2ID[3]);
-	printf("-> SubChunk2Size = %d  \n\n", header_p->Subchunk2Size);
+	printf("\n");
+	printf("-> ChunkID = %c%c%c%c \n", header.ChunkID[0], header.ChunkID[1], header.ChunkID[2], header.ChunkID[3]);
+	printf("-> ChunkSize = %d \n", header.ChunkSize);
+	printf("-> Chunk Format = %c%c%c%c \n", header.Format[0], header.Format[1], header.Format[2], header.Format[3]);
+	printf("-> Subchunk1ID = %c%c%c \n", header.Subchunk1ID[0], header.Subchunk1ID[1], header.Subchunk1ID[2]);
+	printf("-> Subchunk1size = %d \n", header.Subchunk1Size);
+	printf("-> AudioFormat = %d \n", header.AudioFormat);
+	printf("-> Number of channels = %d \n", header_p ->NumChannels);
+	printf("-> Sample rate = %d \n", header_p ->SampleRate);
+	printf("-> ByteRate = %d \n", header_p ->ByteRate);
+	printf("-> BlockAlign = %d \n", header_p ->BlockAlign);
+	printf("-> BitsPerSample = %d \n", header_p ->BitsPerSample);
+	printf("-> SubChuck2ID = %c%c%c%c \n", header.Subchunk2ID[0], header.Subchunk2ID[1], header.Subchunk2ID[2], header.Subchunk2ID[3]);
+	printf("-> SubChunk2Size = %d  \n\n", header.Subchunk2Size);
 
+	return 0;
 }
-	 return 1;
+
+int write_pcm_wavefile(pcm_wavefile_header_t *header_p, char *data, char *filename_output){ /* I changed the signature from *filename to *filename_output */
+
+	FILE* filevar;		/* variable of type file */
+
+	filevar = fopen(filename_output, "rb");										/* opening the file and storing it in file variable */
+
+	if(filevar == 0){  //If file pointer returns NULL
+	    printf("%3s ERROR: File unknown \n", " ");  //Log Error
+	    if(freopen(filename_output, "wb", filevar) !=0) {
+	        printf("%3s ERROR : Unable to create file", " ");
+	        return -1;
+	    }
+	    printf("\n");
+	}
+
+return 0;
 }
-
-int write_pcm_wavefile(pcm_wavefile_header_t *header_p, char *data, char *filename_output) /* I changed the signature from *filename to *filename_output */
-{
-
-	if(file_p == 0){  //If file pointer returns NULL
-	      printf("%3s ERROR: File unknown \n", " ");  //Log Error
-	          if(freopen(*filename, "wb", file_p) !=0) {
-	            printf("%3s ERROR : Unable to create file", " ");
-	            return -1;
-	          }
-	      printf("\n");
-	  }
-
-return 1;
- }
