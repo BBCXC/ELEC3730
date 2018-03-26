43a2edbf6acf7f8072bece47ffb6c79678901e2c
Unknown <c3206230@uon.edu.au>
2018-03-26 15:03:42 +1100

    Adds 2

---
+++ Assignment_2/Ass-02-WINNT/src/Ass-02-Q02.c
@@ -0,0 +1,12 @@
+//     $Date: 2018-03-26 08:32:18 +1100 (Mon, 26 Mar 2018) $
+// $Revision: 1217 $
+//   $Author: Peter $
+
+#include "Ass-02.h"
+#ifdef STM32F407xx
+#include "usart.h"
+#endif
+
+//
+// REPLACE THE EXAMPLE CODE WITH YOUR CODE
+//
