1b9a6db33806380565f37c47579be464c58ecdaf
Unknown <c3206230@uon.edu.au>
2018-05-16 18:00:52 +1000

    planning

--- Assignment_3/Ass-03-STM32/Src/Ass-03-Q1-Test.c
+++ Assignment_3/Ass-03-STM32/Src/Ass-03-Q1-Test.c
@@ -7,85 +7,75 @@ struct window {
   int line_colour;
   int grid_colour;
 
-  int zoom_coeff = 1;         // Number between 1 and MAX_ZOOM
-  int window_buflen;  // Number of input values that are mapped to the
-                          // window buffer
+  int zoom_coeff = 1;  // Number between 1 and MAX_ZOOM
+  int window_buflen;   // Number of input values that are mapped to the
+                       // window buffer
 
-  int auto_scale = // Holds the maximum value that the window buffer has on the screen
+  int auto_scale =  // Holds the maximum value that the window buffer has on the
+                    // screen
 
-  int next = 0;	// Holds the position in the window buffer array
-  				// that is the latest filled
+      int next = 0;  // Holds the position in the window buffer array
+                     // that is the latest filled
 
 }
 
 void windowInit(){
-	int Window_buffer[window.width][2] = ()calloc();
+  int Window_buffer[window.width][2] = () calloc();
 
-	window.buflen = Max_Samples / zoom_coeff / window.width;
+  window.buflen = Max_Samples / zoom_coeff / window.width;
 }
 
 // Only called when input buffer has been populated with next 40 samples
-int populateWindow_avg(){
+int populateWindow_avg() {
+  // Increment Window buffer position
+  window.next++;
+  if (window.next == window.width) {
+    window.next = 0;
+  }
 
-	// Increment Window buffer position
-	window.next++;
-	if(window.next == window.width){window.next = 0;}
+  // Increment Input buffer position
+  input.next++;
+  if (input.next == Max_Samples) {
+    input.next = 0;
+  }
 
-	// Increment Input buffer position
-	input.next++;
-	if(input.next == Max_Samples){input.next = 0;}
+  // // Alternative to the other max finder
+  // int temp = Window_buffer[window.next][0];
+  // // if the previous max value is in the bucket i am about to overwrite
+  // then re calculate the max
+  // otherwise the max remains
 
-	Window_buffer[window.next][0] = Input_buffer[input.next];
+  int avg_input = Input_buffer[input.next];
 
-	for(int i=1; i<window.buflen; i++){
-		input.next++;
-		Window_buffer[window.next][0] += Input_buffer[input.next];
-	}
+  for (int i = 1; i < window.buflen; i++) {
+    input.next++;
+    avg_input += Input_buffer[input.next];
+  }
+  // I want an int without losing the double division
+  Window_buffer[window.next][0] = (int)(avg_input / (double)window.buflen);
+  // TODO find max in an efficient way not like a caveman (apparently there
+  // isn't one)
+  window.auto_scale = 0;
+  for (int i = 0; i < window.width; i++) {
+    if (Window_buffer[i][0] > window.auto_scale) {
+      window.auto_scale = Window_buffer[i][0];
+    }
+  }
 
-	// TODO find max in an efficient way not like a caveman
-	window.auto_scale = 0;
-	for(int i=0; i<window.width; i++){
-		if(Window_buffer[i][0] > window.auto_scale){
-			window.auto_scale = Window_buffer[i][0];
-		}
-	}
-
-
-	// Window buffer now contains average of last 40 samples
-	return 0;
+  // Window buffer now contains average of last 40 samples
+  return 0;
 }
 
 // If zoom_coeff is changed repopulate the Window_buffer with nex mapping
-int re_populateWindow_avg(){
+int re_populateWindow_avg() {
+  // Figure out how im zooming
+  // Zoom on middle
+  // Zoom on start
+  // Zoom on end
 
-	// Figure out how im zooming
-	// Zoom on middle
-	// Zoom on start
-	// Zoom on end
-
-	// Increment from 0 to window.width
-	// 
-
-	// // Increment Window buffer position
-	// window.next++;
-	// if(window.next == window.width){window.next = 0;}
-
-	// // Increment Input buffer position
-	// input.next++;
-	// if(input.next == Max_Samples){input.next = 0;}
-
-	// Window_buffer[window.next][0] = Input_buffer[input.next];
-
-	// for(int i=1; i<window.buflen; i++){
-	// 	input.next++;
-	// 	Window_buffer[window.next][0] += Input_buffer[input.next];
-	// }
-
-	// // Window buffer now contains average of last 40 samples
-	// return 0;
+  // Then it's just a matter of doing what is essentially the populate window
+  // avg function over the width of the buffer
 }
-
-
 
 #define num_buttons 12
 struct button {
@@ -95,193 +85,193 @@ struct button {
   int
 }
 
-/*
-        Buttons
-        - Play
-        - Stop
-        - Save
-        - Load
-        - New
-        - Reset (Reset zoom)
-        - Zoom In
-        - Zoom out
-        - Move Left
-        - Move Right
-        - Up arrow
-        - Down arrow
+    /*
+            Buttons
+            - Play
+            - Stop
+            - Save
+            - Load
+            - New
+            - Reset (Reset zoom)
+            - Zoom In
+            - Zoom out
+            - Move Left
+            - Move Right
+            - Up arrow
+            - Down arrow
 
-*/
+    */
 
-| = 2px
-| 64px|     250px      |
-********************************** - 2px
-* play*                          *
-*     *                          *
-*     *                          *  ~142px
-* stop*                          *
-*     *0                       10*
-********************************** - 2px
-* save* timestamp  <  >  +  - r  *
-* load* |FILENAME        |    ^  *  ~92px
-* new *           1 of 10    !^  *
-********************************** - 2px
+    |
+    = 2px | 64px | 250px |
+      **********************************-2px * play * *******~142px * stop *
+              ***0 10 * **********************************-2px * save *
+              timestamp<> +
+          -r * *load *
+      | FILENAME |
+      ^*~92px * new *
+          1 of 10 !^***********************************-2px
 
+          // Decide what things are displayed
+          // Store that information somewhere
+          // TODO
+          void
+          LCDPopulate() {
+  // TODO below has been commented out, hard coding for now, should be done on
+  // the fly int display_height int display_width
 
-// Decide what things are displayed
-// Store that information somewhere
-// TODO
-void LCDPopulate(){
-	// TODO below has been commented out, hard coding for now, should be done on the fly
-	// int display_height
-	// int display_width
+  window.position[1] = display_width - display_line;       // 318
+  window.position[0] = window.position[1] - window.width;  // 68
+  window.position[2] = display_line;                       // 2
+  window.position[3] = window_height;                      // 144
 
-	window.position[1] = display_width - display_line; // 318
-	window.position[0] = window.position[1] - window.width; // 68
-	window.position[2] = display_line; // 2
-	window.position[3] = window_height; // 144
+  // int button_width = window.position[0] - display_line;
 
-	// int button_width = window.position[0] - display_line;
+  // // Populate left and side buttons x_min x_max
+  // for(int i=0; i<5; i++){
+  // 	button.position[i][0] = display_line;
+  // 	button.position[i][1] = button_width;
+  // }
 
-	// // Populate left and side buttons x_min x_max
-	// for(int i=0; i<5; i++){
-	// 	button.position[i][0] = display_line;
-	// 	button.position[i][1] = button_width;
-	// }
+  // // Populate Play and Stop y_min y_max
+  // int temp1 = 2
+  // for(int i=0; i<temp1; i++){
+  // 	button.position[i][2] =	display_line + (i * (window_height -
+  // display_line) / temp1); 	button.position[i][3] = display_line + ((i+1) *
+  // (window_height - display_line) / temp1);
+  // }
 
-	// // Populate Play and Stop y_min y_max
-	// int temp1 = 2
-	// for(int i=0; i<temp1; i++){
-	// 	button.position[i][2] =	display_line + (i * (window_height - display_line) / temp1);
-	// 	button.position[i][3] = display_line + ((i+1) * (window_height - display_line) / temp1);
-	// }
+  // // Populate Save Load New y_min y_max
+  // int temp2 = 3;
+  // int panel_height = 3 * display_line - window.width;
+  // for(int i=temp1; i<temp2; i++){
+  // 	button.position[i][2] =	display_line + (i * (panel_height -
+  // display_line) / temp2); 	button.position[i][3] = display_line + ((i+1) *
+  // (panel_height - display_line) / temp2);
+  // }
 
-	// // Populate Save Load New y_min y_max
-	// int temp2 = 3;
-	// int panel_height = 3 * display_line - window.width;
-	// for(int i=temp1; i<temp2; i++){
-	// 	button.position[i][2] =	display_line + (i * (panel_height - display_line) / temp2);
-	// 	button.position[i][3] = display_line + ((i+1) * (panel_height - display_line) / temp2);
-	// }
+  // Tempted to hardcode these values, maybe there's a better way
+  // TODO Remove hard coded values
 
+  button.item[0] = "Play";
+  button.position[0][0] = 2;
+  button.position[0][1] = 66;
+  button.position[0][2] = 2;
+  button.position[0][3] = 70;
 
-	// Tempted to hardcode these values, maybe there's a better way
-	// TODO Remove hard coded values
+  button.item[1] = "Stop";
+  button.position[1][0] = 2;
+  button.position[1][1] = 66;
+  button.position[1][2] = 68;
+  button.position[1][3] = 144;
 
-	button.item[0] = "Play";
-	button.position[0][0] = 2;
-	button.position[0][1] = 66;
-	button.position[0][2] = 2;
-	button.position[0][3] = 70;
+  button.item[2] = "Save";
+  button.position[2][0] = 2;
+  button.position[2][1] = 66;
+  button.position[2][2] = 146;
+  button.position[2][3] = 176;
 
-	button.item[1] = "Stop";
-	button.position[1][0] = 2;
-	button.position[1][1] = 66;
-	button.position[1][2] = 68;
-	button.position[1][3] = 144;
+  button.item[3] = "Load";
+  button.position[3][0] = 2;
+  button.position[3][1] = 66;
+  button.position[3][2] = 178;
+  button.position[3][3] = 208;
 
-	button.item[2] = "Save";
-	button.position[2][0] = 2;
-	button.position[2][1] = 66;
-	button.position[2][2] = 146;
-	button.position[2][3] = 176;
+  button.item[4] = "New";
+  button.position[4][0] = 2;
+  button.position[4][1] = 66;
+  button.position[4][2] = 210;
+  button.position[4][3] = 238;
 
-	button.item[3] = "Load";
-	button.position[3][0] = 2;
-	button.position[3][1] = 66;
-	button.position[3][2] = 178;
-	button.position[3][3] = 208;
+  button.item[5] = "<";
+  button.position[5][0] = 170;
+  button.position[5][1] = 198;
+  button.position[5][2] = 146;
+  button.position[5][3] = 176;
 
-	button.item[4] = "New";
-	button.position[4][0] = 2;
-	button.position[4][1] = 66;
-	button.position[4][2] = 210;
-	button.position[4][3] = 238;
+  button.item[6] = ">";
+  button.position[6][0] = 200;
+  button.position[6][1] = 228;
+  button.position[6][2] = 146;
+  button.position[6][3] = 176;
 
-	button.item[5] = "<";
-	button.position[5][0] = 170;
-	button.position[5][1] = 198;
-	button.position[5][2] = 146;
-	button.position[5][3] = 176;
+  button.item[7] = "+";
+  button.position[7][0] = 230;
+  button.position[7][1] = 258;
+  button.position[7][2] = 146;
+  button.position[7][3] = 176;
 
-	button.item[6] = ">";
-	button.position[6][0] = 200;
-	button.position[6][1] = 228;
-	button.position[6][2] = 146;
-	button.position[6][3] = 176;
+  button.item[8] = "-";
+  button.position[8][0] = 260;
+  button.position[8][1] = 288;
+  button.position[8][2] = 146;
+  button.position[8][3] = 176;
 
-	button.item[7] = "+";
-	button.position[7][0] = 230;
-	button.position[7][1] = 258;
-	button.position[7][2] = 146;
-	button.position[7][3] = 176;
+  button.item[9] = "r";
+  button.position[9][0] = 290;
+  button.position[9][1] = 318;
+  button.position[9][2] = 146;
+  button.position[9][3] = 176;
 
-	button.item[8] = "-";
-	button.position[8][0] = 260;
-	button.position[8][1] = 288;
-	button.position[8][2] = 146;
-	button.position[8][3] = 176;
+  button.item[10] = "^";
+  button.position[10][0] = 290;
+  button.position[10][1] = 318;
+  button.position[10][2] = 178;
+  button.position[10][3] = 208;
 
-	button.item[9] = "r";
-	button.position[9][0] = 290;
-	button.position[9][1] = 318;
-	button.position[9][2] = 146;
-	button.position[9][3] = 176;
+  button.item[11] = "!^";
+  button.position[11][0] = 290;
+  button.position[11][1] = 318;
+  button.position[11][2] = 210;
+  button.position[11][3] = 238;
 
-	button.item[10] = "^";
-	button.position[10][0] = 290;
-	button.position[10][1] = 318;
-	button.position[10][2] = 178;
-	button.position[10][3] = 208;
+  button.item[12] = "";  // Timestamp
+  button.position[12][0] = 68;
+  button.position[12][1] = 168;
+  button.position[12][2] = 146;
+  button.position[12][3] = 176;
 
-	button.item[11] = "!^";
-	button.position[11][0] = 290;
-	button.position[11][1] = 318;
-	button.position[11][2] = 210;
-	button.position[11][3] = 238;
+  button.item[13] = "";  // FileName
+  button.position[13][0] = 68;
+  button.position[13][1] = 288;
+  button.position[13][2] = 178;
+  button.position[13][3] = 208;
 
-	button.item[12] = "";	//Timestamp
-	button.position[12][0] = 68;
-	button.position[12][1] = 168;
-	button.position[12][2] = 146;
-	button.position[12][3] = 176;
-
-	button.item[13] = "";	//FileName
-	button.position[13][0] = 68;
-	button.position[13][1] = 288;
-	button.position[13][2] = 178;
-	button.position[13][3] = 208;
-
-	button.item[14] = "";	//FileNumber
-	button.position[14][0] = 68;
-	button.position[14][1] = 288;
-	button.position[14][2] = 210;
-	button.position[14][3] = 238;
+  button.item[14] = "";  // FileNumber
+  button.position[14][0] = 68;
+  button.position[14][1] = 288;
+  button.position[14][2] = 210;
+  button.position[14][3] = 238;
 }
 
 // Draw window buffer to screen
 // Scale y values based on auto scale
 // DONE
-void graphWindow(){
+void graphWindow() {
+  int element = window.next;
 
-	int element = window.next;
-	
-	int Xpos = window.position[0];
-	// Scale window buffer to fill the height based on the maximum value in window buffer
-	int Ypos = (Window_buffer[element][0] / window.auto_scale) * window.height;
-	
-	// TODO add mutex wait
-	LCD_DrawPixel(Xpos, Ypos, window.line_colour);
-	// TODO add mutex release
+  int Xpos = window.position[0];
+  // Scale window buffer to fill the height based on the maximum value in window
+  // buffer
+  int Ypos = (Window_buffer[element][0] / window.auto_scale) * window.height;
 
-	for(int i=Xpos + 1; i<window.position[1]; i++){
-		element++;
-		if(element == window.width){element = 0;}
+  // TODO add mutex wait
+  LCD_DrawPixel(Xpos, Ypos, window.line_colour);
+  // TODO add mutex release
 
-		// Scale window buffer to fill the height based on the maximum value in window buffer
-		Ypos = (Window_buffer[element][0] / window.auto_scale) * window.height;
-		// TODO add mutex wait
-		LCD_DrawPixel(i, Ypos, window.line_colour);
-		// TODO add mutex release
-	}
-	// TODO add some function that prints the values based on some colour gradient?
+  for (int i = Xpos + 1; i < window.position[1]; i++) {
+    element++;
+    if (element == window.width) {
+      element = 0;
+    }
+
+    // Scale window buffer to fill the height based on the maximum value in
+    // window buffer
+    Ypos = (Window_buffer[element][0] / window.auto_scale) * window.height;
+    // TODO add mutex wait
+    LCD_DrawPixel(i, Ypos, window.line_colour);
+    // TODO add mutex release
+  }
+  // TODO add some function that prints the values based on some colour
+  // gradient?
 }
--- Assignment_3/Ass-03-STM32/Src/Task Plan.c
+++ Assignment_3/Ass-03-STM32/Src/Task Plan.c
@@ -1,17 +1,257 @@
-void Task_01(){
+void Task_01() {
+    // Reads touch panel input based on a timer and draws a dot on the LCD screen
+    // and send a message to Task 2.
 
-	// Check for touch
-	// If touched
-		// Find what was touched
-		// 
-	// Else do nothing
+    // Check for touch
+    if (touched every X) {
+        // Find what was touched
+        if (play_button) {
+            State_Thread1 = State_PLAY;
+        }
+        else if (stop_button) {
+            State_Thread1 = State_STOP;
+        }
+        else if (save_button) {
+            // call what is essentially the stop function
+            // on save ask if user wants to overwrite the current file in the menu
+            State_Thread1 = State_STOP;
+            if (overwrite_button) {
+                // save file name
+                // rm current file
+                // MUTEX ON INPUT BUFFER
+                osMutexWait(inputbuf_Handle, osWaitForever);
+                // save file with saved name
+                // MUTEX OFF INPUT BUFFER
+                osMutexRelease(inputbuf_Handle);
+            }
+            else if (new_button) {
+                // MUTEX ON INPUT BUFFER
+                osMutexWait(inputbuf_Handle, osWaitForever);
+                // save file with name
+                // MUTEX OFF INPUT BUFFER
+                osMutexRelease(inputbuf_Handle);
+            }
+            else if (cancel_button) {
+            }
+            // redraw what was previously on the screen
+        }
+        else if (load_button) {
+            State_Thread1 = State_STOP;
+            // MUTEX ON INPUT BUFFER
+            osMutexWait(inputbuf_Handle, osWaitForever);
+            // read into input buffer
+            // MUTEX ON WINDOW BUFFER
+            osMutexWait(windowbuf_Handle, osWaitForever);
+            // map to window buffer
+            // MUTEX OFF WINDOW BUFFER
+            osMutexRelease(windowbuf_Handle);
+            // MUTEX OFF INPUT BUFFER
+            osMutexRelease(inputbuf_Handle);
+            // send to task 2 to draw window buffer - interrupt
+            State_Thread1 = State_PLAY;
+        }
+
+        else if (zoom function_button) {
+            if (scroll left_button) {
+                // figure out what exactly needs to happen when scrolling
+            }
+            else if (scroll right_button) {
+                // figure out what exactly needs to happen when scrolling
+            }
+            else if (zoom in_button) {
+                Set_zoom_coeff(1);
+            }
+            else if (zoom out_button) {
+                Set_zoom_coeff(-1);
+            }
+            else if (reset zoom_button) {
+                Set_zoom_coeff(0);
+            }
+        }
+        else if (menu up_button) {
+        }
+        else if (menu down_button) {
+        }
+    }
+    // Else do nothing
+    else {
+    }
+
+    switch (State_Thread1) {
+        case State_PLAY:
+            // take the interrupt from the dma
+            // MUTEX ON INPUT BUFFER
+            osMutexWait(inputbuf_Handle, osWaitForever);
+            // store the 40 samples into the input buffer
+            // MUTEX ON WINDOW BUFFER
+            osMutexWait(windowbuf_Handle, osWaitForever);
+            // map values to window buffer
+            if (populateWindow_avg() != 0) {
+                printf("%sERROR:%s Could not map input buffer to window buffer\n", ERROR_M, DEFAULT_COLOUR_M);
+            }
+            // MUTEX OFF WINDOW BUFFER
+            osMutexRelease(windowbuf_Handle);
+            // MUTEX OFF INPUT BUFFER
+            osMutexRelease(inputbuf_Handle);
+
+            // draw window buffer - interrupt
+            // TODO this may have to be 5hz or something instead of 25hz
+            break;
+        case State_STOP:
+            // disregard the interrupt given by the dma
+            break;
+    }
 }
 
-void Task_02(){
+// Only called when input buffer has been populated with next 40 samples
+int populateWindow_avg() {
+    // Increment Window buffer position
+    osMutexWait(inputbuf_Handle, osWaitForever);
+    osMutexWait(windowbuf_Handle, osWaitForever);
+    window.next++;
+    if (window.next == window.width) {
+        window.next = 0;
+    }
 
-	// If message from task 1
-		// Do something
+    // Increment Input buffer position
+    input.next++;
+    if (input.next == Max_Samples) {
+        input.next = 0;
+    }
 
-	// If message from DMA
-		// 
+    // // Alternative to the other max finder
+    // int temp = Window_buffer[window.next][0];
+    // // if the previous max value is in the bucket i am about to overwrite
+    // then re calculate the max
+    // otherwise the max remains
+
+    int avg_input = Input_buffer[input.next];
+
+    for (int i = 1; i < window.buflen; i++) {
+        input.next++;
+        avg_input += Input_buffer[input.next];
+    }
+    osMutexRelease(inputbuf_Handle);
+    // I want an int without losing the double division
+    Window_buffer[window.next][0] = (int) (avg_input / (double) window.buflen);
+
+    window.auto_scale = 0;
+    for (int i = 0; i < window.width; i++) {
+        if (Window_buffer[i][0] > window.auto_scale) {
+            window.auto_scale = Window_buffer[i][0];
+        }
+    }
+    osMutexRelease(windowbuf_Handle);
+
+    // Window buffer now contains average of last 40 samples
+    return 0;
+}
+
+void Set_zoom_coeff(int direction) {
+    // Need to decide where this fits best.
+    // Maybe should be contained within thread 1 and only the window buffer passed
+    // to thread 2
+    osMutexWait(windowbuf_Handle, osWaitForever);
+    if (direction == 0) {
+        // reset zoom
+        window.zoom_coeff = 0;
+    }
+    else if (direction == -1) {
+        // zoom out
+        if (window.zoom_coeff != MIN_ZOOM) {
+            window.zoom_coeff += -1;
+        }
+    }
+    else if (direction == 1) {
+        // zoom in
+        if (window.zoom_coeff != MAX_ZOOM) {
+            window.zoom_coeff += 1;
+        }
+    }
+    osMutexRelease(windowbuf_Handle);
+
+    // Re populate window
+    // Redraw window
+}
+
+// Thread 2
+void Task_02() {
+    // Waits for a message from Task 1 and displays the result.
+
+    // If signal from task 1
+    // this might be message or signal
+    // Do something
+    GraphProcess();
+    // Each time you draw the graph draw the BPM over the top of the graph
+    Draw_BMP();
+}
+
+void ClearWindow() {
+    // Get the min and max window parts
+    osMutexWait(windowbuf_Handle, osWaitForever);
+    int x_min = window.position[0];
+    int x_max = window.position[1];
+    int y_min = window.position[2];
+    int y_max = window.position[3];
+    osMutexRelease(windowbuf_Handle);
+
+    int windowlen    = x_max - x_min osMutexWait(myMutex01Handle, osWaitForever);
+    uint32_t counter = 0;
+
+    BSP_LCD_SetTextColor(LCD_COLOUR_WHITE);
+
+    for (counter = y_min; counter < y_max; counter++) {
+        BSP_LCD_DrawHLine(x_min, counter, windowlen);
+    }
+
+    BSP_LCD_SetTextColor(LCD_COLOR_BLACK);
+    osMutexRelease(myMutex01Handle);
+}
+
+void GraphProcess() {
+    // take the window buffer
+    // graph it
+    osMutexWait(windowbuf_Handle, osWaitForever);
+
+    int x_min = window.position[0];
+    int x_max = window.position[1];
+    int y_min = window.position[2];
+    int y_max = window.position[3];
+
+    int line_colour = window.line_colour;
+    // start index of the data
+    int element = window.next;
+
+    int Xpos = window.position[0];
+    // Scale window buffer to fill the height based on the maximum value in window
+    // buffer
+
+    int Ypos = (Window_buffer[element][0] / window.auto_scale) * window.height;
+
+    for (int i = Xpos; i < window.position[1]; i++) {
+        // Scale window buffer to fill the height based on the maximum value in
+        // window buffer
+        Ypos = (Window_buffer[element][0] / window.auto_scale) * window.height;
+        // TODO add mutex wait
+        osMutexWait(myMutex01Handle, osWaitForever);
+        // erase old dot
+        BSP_LCD_SetTextColor(LCD_COLOUR_WHITE);
+        BSP_LCD_DrawVLine(x_min + element, y_min, y_max - y_min);
+        LCD_DrawPixel(i, Ypos, line_colour);
+        // TODO add mutex release
+        osMutexRelease(myMutex01Handle);
+
+        element++;
+        if (element == window.width) {
+            element = 0;
+        }
+    }
+    // TODO add some function that prints the values based on some colour
+    // gradient?
+    osMutexRelease(windowbuf_Handle);
+}
+
+void Draw_BMP() {
+    // take given BMP
+    // draw in given spot
 }
