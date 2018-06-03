7bf428c866098e169aab49665b75752575692e07
Unknown <c3206230@uon.edu.au>
2018-06-03 11:27:25 +1000

    .
    .

---
+++ Assignment_3/Ass-03-STM32-V03/Src/Assignment_3_3730.c
@@ -0,0 +1,28 @@
+Assignment 3
+
+Question 1
+	Debug on off
+	analog <time> change period of time to plot
+	ls list the contents of folder
+	cd <dir> 
+	mkdir <dir>
+	cp <src> <dst> consider what happens if the dst already exists
+	rm <file> consider cases where the file name does not exist or that the name is a folder name
+
+	TODO for all terminal commands hand;e invalid folder names
+	TODO Display current path in command prompt
+
+Question 2
+	Start
+	Stop
+	Save 3 sets of recorded data
+		My thoughts
+			Save timestamp
+			Save Scale of data for use when reading back
+	Load stored data to display
+	Display scale of data along the bottom
+
+	Extra
+		BMP
+		Zoom
+		Scale
